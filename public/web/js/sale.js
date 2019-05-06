23590b7aecee9fb7919998286a5406899d039f42
mbourdin <bourdin.maurice@gmail.com>
2019-05-06 16:06:08 +0200

    validation formulaire commande + adresse

--- public/web/js/sale.js
+++ public/web/js/sale.js
@@ -7,3 +7,44 @@ function submitform()
 {
     document.forms["form"].submit();
 }
+
+function isEmpty(id)
+{   console.log(id);
+    val=document.getElementById(id);
+    return val.value===undefined || val.value==="";
+}
+
+function buttonEnabler()
+{   button=document.getElementById("submitbutton");
+
+
+    recipientIsEmpty=( document.getElementById("recipient").value ===undefined || document.getElementById("recipient").value==="" );
+    numeroIsEmpty=(document.getElementById("number").value==="" || document.getElementById("number").value<0);
+
+    if
+    (
+        !isEmpty("recipient")
+        &&
+        (    document.getElementById("useownaddress").checked
+
+            ||
+            (
+                !isEmpty("number")
+                &&
+                !isEmpty("roadname")
+                &&
+                !isEmpty("cityOptions")
+            )
+        )
+    )
+    {
+        button.disabled = false;
+        console.log("enable")
+    }
+    else
+    {   console.log("disable");
+        button.disabled=true;
+    }
+
+    console.log("useownaddres"+ document.getElementById("useownaddress").value);
+}
